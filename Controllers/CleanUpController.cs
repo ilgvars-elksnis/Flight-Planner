476bbeea3572872bf0324d3a472fa5b8a05a9086
ilgvars-elksnis <Lion150@inbox.lv>
2023-10-11 16:14:49 +0300

    rest

---
+++ Controllers/CleanUpController.cs
@@ -0,0 +1,25 @@
+﻿using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("testing-api")]
+    [ApiController]
+    public class CleanUpController : ControllerBase
+    {
+        private readonly FlightStorage _storage;
+        public CleanUpController ()
+        {
+            _storage = new FlightStorage ();
+        }
+
+        [Route("clear")]
+        [HttpPost]
+        public IActionResult Clear() 
+        {
+            _storage.Clear ();
+            return Ok();
+        }
+    }
+}
